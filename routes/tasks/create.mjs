b9e2240bf654bcff05e4f41dc0ea8d6a240c6a58
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-10 22:55:03 -0300

    fiap

--- routes/tasks/create.mjs
+++ routes/tasks/create.mjs
@@ -4,6 +4,6 @@ import { openDb } from './load.mjs'
 export async function createTask(payload) {
     const db = await openDb()
     console.log(`Creating task.... ${payload.title}`)
-    db.run("INSERT INTO tasks VALUES (?, ?)", [payload.id, payload.title]);   
+    db.run("INSERT INTO tasks (id, title, completed_at) VALUES (?, ?, ?)", [payload.id, payload.title, payload.completed_at]);   
     db.close();
 }
